8285fac4c3ed28870200d658cd557f0db5a9445b
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-05-21 01:21:55 +0530

    reverting back all changes

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -13,13 +13,7 @@ class HomePage extends StatelessWidget {
     String name = "Coddepur";
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0.0,
-        iconTheme: IconThemeData(color: Colors.black),
-        title: Text(
-          "Catalog App",
-          style: TextStyle(color: Colors.black),
-        ),
+        title: Text("Catalog App"),
       ),
       body: Center(
         child: Container(
