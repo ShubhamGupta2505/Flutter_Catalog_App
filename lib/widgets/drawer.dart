d802221a2e3a7b014c7255085bcd8a2f4ac9adfe
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-05-20 22:24:42 +0530

    adding image account

--- lib/widgets/drawer.dart
+++ lib/widgets/drawer.dart
@@ -5,15 +5,19 @@ class MyDrawer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final imageUrl =
+        "https://github.com/ShubhamGupta2505/React_Starter_Basic/blob/main/nick.jpg?raw=true";
     return Drawer(
       child: ListView(
         children: [
           DrawerHeader(
               padding: EdgeInsets.zero,
               child: UserAccountsDrawerHeader(
-                  margin: EdgeInsets.zero,
-                  accountName: Text("Shubham Gupta"),
-                  accountEmail: Text("shubham3nemo@gmail.com")))
+                margin: EdgeInsets.zero,
+                accountName: Text("Shubham Gupta"),
+                accountEmail: Text("shubham3nemo@gmail.com"),
+                currentAccountPicture: Image.network(imageUrl),
+              ))
         ],
       ),
     );
