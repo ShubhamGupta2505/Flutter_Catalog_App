0c6d867b8c9cf983ca989a46aa9c95b4fffc1936
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-05-22 02:31:46 +0530

    saving current

--- lib/main.dart
+++ lib/main.dart
@@ -21,16 +21,17 @@ class MyApp extends StatelessWidget {
       // home: HomePage(),
       themeMode: ThemeMode.light,
       theme: ThemeData(
-          primarySwatch: Colors.deepPurple,
-          // fontFamily: GoogleFonts.lato().fontFamily,
-          appBarTheme: AppBarTheme(
-              color: Colors.white,
-              elevation: 0.0,
-              iconTheme: IconThemeData(
-                color: Colors.black)
-                )
-                ),
+        primarySwatch: Colors.deepPurple,
+        // fontFamily: GoogleFonts.lato().fontFamily,
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0.0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+        // textTheme: Theme.of(context).textTheme
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
+
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
