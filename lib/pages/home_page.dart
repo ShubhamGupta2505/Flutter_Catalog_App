be457fc7ea6d1c356a53b7d1dd47f865a5597e6e
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-05-22 15:56:27 +0530

    creating loaddata function in init function

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,14 +6,31 @@ import 'package:hello_rectangle/models/catalog.dart';
 import '../widgets/drawer.dart';
 import '../widgets/item_widget.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    loadData();
+  }
+
+  loadData() {
+    
+  }
 
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(12, (index) => CatalogModel.items[0]);
     int days = 34;
     String name = "Coddepur";
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
