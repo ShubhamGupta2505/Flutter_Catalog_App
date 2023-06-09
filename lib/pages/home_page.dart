a00a04dc1f1203527889414b9c2fce5a7e8dd2f2
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-06-09 15:55:21 +0530

    next cataloglist creation

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -45,14 +45,23 @@ class _HomePageState extends State<HomePage> {
         body: SafeArea(
       child: Container(
         padding: Vx.m32,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            "Catalog App".text.xl5.bold.color(MyTheme.darkBluisColor).make(),
-            "Trending Products".text.xl2.make(),
-          ],
-        ),
+        child: CatalogHeader(),
       ),
     ));
+  }
+}
+
+class CatalogHeader extends StatelessWidget {
+  const CatalogHeader({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        "Catalog App".text.xl5.bold.color(MyTheme.darkBluisColor).make(),
+        "Trending Products".text.xl2.make(),
+      ],
+    );
   }
 }
