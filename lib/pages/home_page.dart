2262ea4d02439874620c704cfa1af649bf33235b
ShubhamGupta2505 <shubham3nemo@gmail.com>
2023-06-10 10:09:40 +0530

    extracting catalogheader

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -65,20 +65,7 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-class CatalogHeader extends StatelessWidget {
-  const CatalogHeader({Key? key}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        "Catalog App".text.xl5.bold.color(MyTheme.darkBluisColor).make(),
-        "Trending Products".text.xl2.make(),
-      ],
-    );
-  }
-}
 
 class CatalogList extends StatelessWidget {
   const CatalogList({Key? key}) : super(key: key);
